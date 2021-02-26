3d2bf81774f9d4b24cbadab5eebe39f3768f6e91
1121512088@qq.com <1121512088@qq.com>
2021-02-26 15:32:16 +0800

    update demo2

--- demo2/src/index.js
+++ demo2/src/index.js
@@ -1,17 +1,21 @@
-import _ from "Lodash";
-import printMe from "./print";
+import "./index.less";
 
 function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+  var element = document.createElement('pre');
 
-  element.innerHTML = _.join(['sample', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click console!';
-  btn.onclick = printMe;
-  element.appendChild(btn);
+  element.innerHTML = ['sample', 'webpack', "1233"].join('\n\n');
 
   return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // 当 index.js 改变导致页面重新渲染时，重新获取渲染的元素
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept('./index.js', function () {
+    console.log("监听热: print");
+    document.body.removeChild(element);
+    element = component(); // 重新渲染页面后，component 更新 click 事件处理
+    document.body.appendChild(element);
+  })
+}
--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -1,15 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
 
 module.exports = {
   entry: {
     app: "./src/index.js",
-    print: "./src/print.js"
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(css|less)$/,
+        use: [
+          "style-loader", // 顺序先
+          "css-loader",
+          "less-loader",
+        ]
+      },
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({ // 生成html 到 dist下
@@ -18,9 +31,11 @@ module.exports = {
     new CleanWebpackPlugin({  // 打包前清空 dist
       cleanStaleWebpackAssets: false // 在webpack --watch 模式下 默认会将没有改变的html文件清除
     }),
+    new HotModuleReplacementPlugin(),
   ],
   devtool: 'inline-source-map',
   devServer: { // 配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件
     contentBase: './dist',
+    hot: true,
   }
 }
