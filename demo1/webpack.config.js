ed6b8d7512497a474a10cc55eac7752b5bd872e2
1121512088@qq.com <1121512088@qq.com>
2021-06-15 18:01:49 +0800

    update

--- demo1/webpack.config.js
+++ demo1/webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true, // webpack >= 5.20 才有用 清理 /dist 文件夹 
+    // webpack5 => 默认情况下 asset/resource(图片，文字)模块以 [hash][ext][query] 文件名发送到输出目录
+    assetModuleFilename: 'images/[hash][ext][query]',
   },
   mode: "production",
   module: {
@@ -40,6 +42,16 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      // svg 使用方式
+      // import metroMap from './images/metro.svg';
+      // block.style.background = `url(${metroMap})`
+      // 所有 .svg 文件都将作为 data URI 注入到 bundle 中
+      // inline 资源
+      {
+        test: /\.svg/,
+        type: 'asset/inline'
+      },
+      
     ]
   },
 }
